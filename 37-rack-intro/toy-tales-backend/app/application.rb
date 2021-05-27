1c2883ce715d1e240aef526b9ff087aae871b206
Michelle Rios <brendamichelle995@gmail.com>
2021-05-27 00:15:01 -0400

    before lecture

--- 37-rack-intro/toy-tales-backend/app/application.rb
+++ 37-rack-intro/toy-tales-backend/app/application.rb
@@ -5,6 +5,8 @@ class Application
 
     if req.path.match(/hello/)
       return [200, { "Content-Type" => "application/json" }, [{ :message => "hello world!" }.to_json]]
+    else
+      [404, {}, ["Path not found!!!"]]
     end
   end
 end
